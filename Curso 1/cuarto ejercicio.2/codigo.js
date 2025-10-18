e19ecc0a9bcce2202afce56c44f50e524041c3e5
Hades <adesea2209@gmail.com>
2025-10-18 16:08:05 -0700

    Cuarto ejercicio 2

--- Curso 1/cuarto ejercicio.2/codigo.js
+++ Curso 1/cuarto ejercicio.2/codigo.js
@@ -1,3 +1,6 @@
+// Crear función para preguntar a qué materia se quiere inscribir
+// Si ya hay 20 alumnos anotados negar la inscripción
+// Si hay menos inscribir y añadir a la lista de alumnos
 const materias = {
     fisica: ["Perez","pedro","pepito","cofla","maria"],
     programacion: ["Dalto","pedro","juan","pepito"],
@@ -5,7 +8,17 @@ const materias = {
     quimica: ["Rodriguez","pedro","juan","pepito","cofla","maria"]
 };
 
-// Crear función para preguntar a qué materia se quiere inscribir
-// Si ya hay 20 alumnos anotados negar la inscripción
-// Si hay menos inscribir y añadir a la lista de alumnos
-const 
+const incripcionMateria = (alumno,materia) => {
+
+    if(materias[materia].length >= 21){
+        console.log(`Lo siento ${alumno}, las clases de ${materia} ya están llenas.`);
+    } else{
+        materias[materia].push(alumno);
+        console.log(`Felicidades ${alumno}, te has inscrito a ${materia}.`);
+        console.log(materias);
+    }
+}
+
+incripcionMateria("Cofla","fisica");
+incripcionMateria("Maria","quimica");
+incripcionMateria("Pepito","programacion");
